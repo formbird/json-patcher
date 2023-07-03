7b679a8bc600a4e91c4b36fbdb44edd4716c8233
Muhammad Hamza <muhammadhamza1311@gmail.com>
2023-07-03 22:23:47 +0500

    Attempt to run tests on windows

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -7,8 +7,8 @@ import { importMapsPlugin } from '@web/dev-server-import-maps';
 
 const commonJs = fromRollup(rollupCommonJs);
 
-const tsConfigPath = join(process.cwd(), 'test/tsconfig.json')
-const chaiPath = join(process.cwd(), 'node_modules/@esm-bundle/chai/esm/chai.js');
+const tsConfigPath = join(process.cwd(), 'test', 'tsconfig.json')
+const chaiPath = join(process.cwd(), 'node_modules', '@esm-bundle', 'chai', 'esm', 'chai.js');
 
 export default {
     nodeResolve: {
